9608c906ad3233178e1b64c791597a957c7ab7f8
Sherman Perez <shermanperez0@gmail.com>
2025-01-21 16:37:39 -0400

    gun shoots to the mouse position

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,24 +1,11 @@
 extends Node2D
 
-const BULLET = preload("res://scenes/bullet.tscn")
+const BULLET = preload("res://scenes/Character/bullet.tscn")
 @onready var muzzle: Marker2D = $Marker2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	
-	rotation_degrees = wrap(rotation_degrees, 0, 360)
-	
-	if rotation_degrees > 90 and rotation < 270:
-		scale.y = -1
-	else:
-		scale.y = 1
-	
+
 	if Input.is_action_pressed("fire"):
 		shoot()
 
