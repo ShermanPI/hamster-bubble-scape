84ba4ea0c47535c64557e2a2925c50ad5dc4cb46
Sherman Perez <shermanperez0@gmail.com>
2025-01-26 15:39:53 -0400

    music added and thanks for watching

---
+++ scenes/levels/scripts/level_4.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GlobalVariables.levelString = "/root/Level4/GameManager"
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_static_body_2d_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
