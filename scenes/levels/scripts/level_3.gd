fe1ccb4b7267682b641d3321f0864e8e8dc8eaad
Sherman Perez <shermanperez0@gmail.com>
2025-01-26 15:17:44 -0400

    added spikes

--- scenes/levels/scripts/level_3.gd
+++ scenes/levels/scripts/level_3.gd
@@ -8,3 +8,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_static_body_2d_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
