09fa8e7b0dc4515ce4a208a714a9e16eb1037999
Sherman Perez <shermanperez0@gmail.com>
2025-01-25 23:20:41 -0400

    added hamster animation sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var player_shooted = false
 @onready var game_manager: Node = %GameManager
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,9 +16,14 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#Get the input direction: -1 or 0 or 1
 	var direction := Input.get_axis("ui_left", "ui_right")
+
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
